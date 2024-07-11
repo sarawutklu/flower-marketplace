d42c23d387b887752e96994cb8a4a273d2c2427c
sarawutklu <sarawut.klu@gmail.com>
2024-07-12 02:20:31 +0700

    update fe

--- flower-marketplace/src/app/pages/update-flower/update-flower.module.ts
+++ flower-marketplace/src/app/pages/update-flower/update-flower.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UpdateFlowerComponent } from './update-flower.component';
 import { UpdateFlowerRoutes } from './update-flower.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    UpdateFlowerRoutes
+    UpdateFlowerRoutes,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [UpdateFlowerComponent]
 })
